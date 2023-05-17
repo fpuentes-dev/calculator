9da6ee445d108098d3b130074dbadf3c7f2852ef
fpuentes <fpuentes@ujaen.es>
2023-05-17 20:06:39 +0200

    fix errors

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
   swcMinify: true,
   webpack: (config) => {
     config.resolve.alias['@src'] = path.resolve(__dirname, 'src')
-    config.resolve.alias['@assets'] = path.resolve(__dirname, 'public')
+    config.resolve.alias['@styles'] = path.resolve(__dirname, 'styles')
+    config.resolve.alias['@public'] = path.resolve(__dirname, 'public')
     return config
   },
 }
