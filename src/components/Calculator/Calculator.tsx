be86284c861a609172f0aa47199179268e3f0087
fpuentes <fpuentes@ujaen.es>
2023-05-21 13:33:17 +0200

    added extra animation and some styles

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -6,17 +6,26 @@ import MainButtons from './components/MainButtons'
 import { ContextCalculator, ContextCalculatorInitial } from './context/ContextCalculator'
 import styles from './styles.module.scss'
 
-const { container, extraButtonsContainer, mainButtonsContainer, display } = styles
+const { container, extraButtonsContainer, mainButtonsContainer, display, extraButton } = styles
 
 const Calculator: React.FC = () => {
+  const [extraOpen, setExtraOpen] = useState(false)
   const [context, setContext] = useState(ContextCalculatorInitial)
+
+  const toggleExtra = () => {
+    setExtraOpen(!extraOpen)
+  }
+
   return (
     <ContextCalculator.Provider value={{ context, setContext }}>
       <div className={container}>
-        <div className={extraButtonsContainer}>
+        <div className={extraButtonsContainer} style={{ width: extraOpen ? 400 : 0, padding: extraOpen ? 10 : 0 }}>
           <ExtraButtons />
         </div>
         <div className={mainButtonsContainer}>
+          <button className={extraButton} onClick={toggleExtra}>
+            *
+          </button>
           <input type='text' className={display} value={context.numAux || context.num || context.result} readOnly />
           <MainButtons />
         </div>
